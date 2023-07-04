c44b3cf096ffd1df52dabe370b843e9eab4b72dc
Liam Risdon <liamrisdon22@gmail.com>
2023-07-04 16:34:02 +0100

    Project set up and first test passed

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -1,2 +1,11 @@
 public class Scrabble {
+
+    String word;
+
+    public Scrabble(String newWord) {
+        this.word = newWord;
+    }
+    public int score() {
+        return 0;
+    }
 }
--- src/test/java/ScrabbleTest.java
+++ src/test/java/ScrabbleTest.java
@@ -12,47 +12,51 @@ public class ScrabbleTest {
         assertEquals(scrabble.score(), 0);
     }
 
-    @Test
-    @Disabled
-    public void returnsZeroForNull() {
-        Scrabble scrabble = new Scrabble(null);
-        assertEquals(scrabble.score(), 0);
-    }
 
-    @Test
-    @Disabled
-    public void returnsScoreForShortWord1() {
-        Scrabble scrabble = new Scrabble("a");
-        assertEquals(scrabble.score(), 1);
-    }
 
-    @Test
-    @Disabled
-    public void returnsScoreForShortWord2() {
-        Scrabble scrabble = new Scrabble("f");
-        assertEquals(scrabble.score(), 4);
-    }
+//    @Test
+//    @Disabled
+//    public void returnsZeroForNull() {
+//        Scrabble scrabble = new Scrabble(null);
+//        assertEquals(scrabble.score(), 0);
+//    }
+//
+//    @Test
+//    @Disabled
+//    public void returnsScoreForShortWord1() {
+//        Scrabble scrabble = new Scrabble("a");
+//        assertEquals(scrabble.score(), 1);
+//    }
+//
+//    @Test
+//    @Disabled
+//    public void returnsScoreForShortWord2() {
+//        Scrabble scrabble = new Scrabble("f");
+//        assertEquals(scrabble.score(), 4);
+//    }
+//
+//    @Test
+//    @Disabled
+//    public void returnsScoreForSimpleWord() {
+//        Scrabble scrabble = new Scrabble("street");
+//        assertEquals(scrabble.score(), 6);
+//    }
+//
+//    @Test
+//    @Disabled
+//    public void returnsScoreForComplicatedWord() {
+//        Scrabble scrabble = new Scrabble("quirky");
+//        assertEquals(scrabble.score(), 22);
+//    }
+//
+//    @Test
+//    @Disabled
+//    public void returnsScoreForCaseInsensitiveWord() {
+//        Scrabble scrabble = new Scrabble("OXYPHENBUTAZONE");
+//        assertEquals(scrabble.score(), 41);
+//    }
 
-    @Test
-    @Disabled
-    public void returnsScoreForSimpleWord() {
-        Scrabble scrabble = new Scrabble("street");
-        assertEquals(scrabble.score(), 6);
-    }
 
-    @Test
-    @Disabled
-    public void returnsScoreForComplicatedWord() {
-        Scrabble scrabble = new Scrabble("quirky");
-        assertEquals(scrabble.score(), 22);
-    }
-
-    @Test
-    @Disabled
-    public void returnsScoreForCaseInsensitiveWord() {
-        Scrabble scrabble = new Scrabble("OXYPHENBUTAZONE");
-        assertEquals(scrabble.score(), 41);
-    }
 
 //    @Test
 //    @Disabled
